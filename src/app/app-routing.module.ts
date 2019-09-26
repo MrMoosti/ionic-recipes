2fafe65f1e299bb6416abe6cfb2efcc13816bf68
Mustafa Yilmaz <mustafa.yilmaz@student.graafschapcollege.nl>
2019-09-26 15:55:46 +0200

    fixed routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AppPage } from 'e2e/src/app.po';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-  { path: 'recipes', children: [
-    { path: '', loadChildren: './recipes/recipes.module#RecipesPageModule'},
-    { path: ':recipe', loadChildren: './recipes/detail/detail.module#DetailPageModule' },
-    { path: 'add', loadChildren: './recipes/add/add.module#AddPageModule' }
-  ]},
+  {
+    path: 'recipes', children: [
+      { path: '', loadChildren: './recipes/recipes.module#RecipesPageModule' },
+      { path: 'add', loadChildren: './recipes/add/add.module#AddPageModule' },
+      { path: ':recipe', loadChildren: './recipes/detail/detail.module#DetailPageModule' }
+    ]
+  },
 ];
 
 @NgModule({
