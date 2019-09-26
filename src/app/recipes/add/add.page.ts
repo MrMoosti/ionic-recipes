d9bebf10ca5c276cff2318d205936b5af53b7d95
Mustafa Yilmaz <mustafa.yilmaz@student.graafschapcollege.nl>
2019-09-26 17:01:41 +0200

    Created logic for Adding a Recipe

--- src/app/recipes/add/add.page.ts
+++ src/app/recipes/add/add.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipesService } from '../recipes.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.page.scss'],
 })
 export class AddPage implements OnInit {
+  recipeName: string;
+  recipeUrl: string;
+  recipeIngredients: string[] = [];
+  recipeInstructions: string[] = [];
 
-  constructor() { }
+  constructor(private rs: RecipesService, private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
+
   }
 
+  addIngredient() {
+
+  }
+
+  addInstruction() {
+
+  }
+
+  addRecipe() {
+
+  }
+
+  cancel() {
+    this.router.navigateByUrl('/recipes');
+  }
 }
